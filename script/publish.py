18693bc399d15f004aa0b5650df6798ff4be016c
Juan Pedro <JuanP.95.torres@gmail.com>
2018-03-10 11:31:04 +0100

    scripsts

--- script/publish.py
+++ script/publish.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import time
+import random as rn
 #client  = Client(, clean_session=True, userdata=None, protocol=MQTTv311, transport=”tcp”)
 
 
@@ -7,5 +8,5 @@ client =mqtt.Client(client_id="pulsera_abuelo")
 client.connect("158.49.112.108", port=8080)
 
 while True:
-  client.publish("GsmClient/2/posi","00.00,15.248")
+  client.publish("GsmClient/2/posi",str(rn.uniform(0.,100.))+","+str(rn.uniform(0.,100.)))
   time.sleep(5)
