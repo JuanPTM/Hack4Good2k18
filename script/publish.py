f2d160be5f7b47d03f82344b44fb342d473eaf67
Juan Pedro <JuanP.95.torres@gmail.com>
2018-03-12 00:06:52 +0100

    updated links on docker-compose

--- script/publish.py
+++ script/publish.py
@@ -13,7 +13,7 @@ client.connect("192.168.0.161", port=8080)
 id = "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b"
 
 client.publish("dbNotification",id+",up")
-
+time.sleep(2)
 for i in range(len(points)):
     client.publish(id+"/pos",str(points[i][0])+","+str(points[i][1]))
     client.publish(id+"/bat",str(bat[i]))
