3622fc880430a20ce7b6207ce139d783e37f515d
Juan Pedro <JuanP.95.torres@gmail.com>
2018-03-11 21:42:45 +0100

    post example

---
+++ script/post.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import json
+import requests
+
+url = "http://192.168.0.161"
+port = 80
+headers = {'Content-Type': 'application/json'}
+
+
+def get_account_info(device_id,lat,lon):
+
+    api_url = '{0}:{2}/checkfence/{1}'.format(url,device_id,port)
+    print(api_url)
+    params= {'lat':lat,'lon':lon}
+    params = json.dumps(params).encode('utf8')
+    response = requests.post(api_url,data=params, headers=headers)
+
+    if response.status_code == 200:
+        return json.loads(response.content.decode('utf-8'))
+    else:
+        return None
+
+print(get_account_info(1,39.78,-6.3))
